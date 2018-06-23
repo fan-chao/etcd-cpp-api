e6902c04a5607c8eab9a4bdfd39846d9fa733394
space <universe35@163.com>
2018-06-23 16:09:53 +0800

    Add VS build solution.

---
+++ msvc/tst/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: F:/thirdparty/etcd/etcd-cpp-api-master/tst
+# Build directory: F:/thirdparty/etcd/etcd-cpp-api-master/msvc/tst
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(etcd_test "etcd_test")
